adfc1c85bb0721a1cbe19fc95da5b68a994ca343
astr1x <lucaslupus1@gmail.com>
2017-04-20 07:42:41 -0300

    Deletando projeto bugado

--- sigtap/src/integracao/src/main/java/br/com/integracaosigtap/connect/RequestSIGTAPFactory.java
+++
@@ -1,9 +0,0 @@
-package main.java.br.com.integracaosigtap.connect;
-
-/**
- * Created by astr1x on 16/04/17.
- */
-public abstract class RequestSIGTAPFactory {
-
-    public abstract RequestSIGTAP create() throws Exception;
-}
--- sigtap/src/main/java/br/ufg/inf/bsusinterface/Barramento.java
+++
@@ -1,5 +0,0 @@
-package br.ufg.inf.bsusinterface;
-
-public interface Barramento {
-
-}
