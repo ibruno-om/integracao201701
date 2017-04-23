fc4c58b53fca21c66795607a08307d544069f1a6
astr1x <lucaslupus1@gmail.com>
2017-04-22 21:49:57 -0300

    Implementacao da classe de requisicao de pesquisar compatibilidades possiveis

--- src/main/java/br/com/integracaosigtap/connect/RequestPesquisarCompatibilidades.java
+++ src/main/java/br/com/integracaosigtap/connect/RequestPesquisarCompatibilidades.java
@@ -1,5 +1,7 @@
 package br.com.integracaosigtap.connect;
 
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPException;
 
 /**
@@ -7,18 +9,45 @@ import javax.xml.soap.SOAPException;
  */
 public class RequestPesquisarCompatibilidades extends RequestSIGTAP {
 
-    static String url = "http://servicoshm.saude.gov.br/sigtap/NivelAgregacaoService/v1?";
+    static String url = "https://servicoshm.saude.gov.br/sigtap/CompatibilidadeService/v1";
 
     protected RequestPesquisarCompatibilidades() throws Exception {
         super(url);
     }
 
     protected void addNameSpace() throws SOAPException {
+        envelope.addNamespaceDeclaration("com", "http://servicos.saude.gov.br/sigtap/v1/compatibilidadeservice");
+        envelope.addNamespaceDeclaration("com1", "http://servicos.saude.gov.br/schema/sigtap/compatibilidade/v1r1/compatibilidadepossivel");
+        envelope.addNamespaceDeclaration("com2", "http://servicos.saude.gov.br/schema/corporativo/v1/competencia");
+        envelope.addNamespaceDeclaration("grup", "http://servicos.saude.gov.br/schema/sigtap/procedimento/nivelagregacao/v1/grupo");
+        envelope.addNamespaceDeclaration("sub", "http://servicos.saude.gov.br/schema/sigtap/procedimento/nivelagregacao/v1/subgrupo");
+        envelope.addNamespaceDeclaration("proc", "http://servicos.saude.gov.br/schema/sigtap/procedimento/v1/procedimento");
+        envelope.addNamespaceDeclaration("pag", "http://servicos.saude.gov.br/wsdl/mensageria/v1/paginacao");
 
     }
 
     @Override
     protected void addContent() throws SOAPException {
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement requestPesquisarCompatibilidades = soapBody.addChildElement("requestPesquisarCompatibilidades", "com");
 
+        SOAPElement codigoCompatibilidadePossivel = requestPesquisarCompatibilidades.addChildElement("codigoCompatibilidadePossivel", "com1");
+        codigoCompatibilidadePossivel.addTextNode("01");
+
+        SOAPElement competencia = requestPesquisarCompatibilidades.addChildElement("competencia", "com2");
+        competencia.addTextNode("200801");
+
+        SOAPElement codigoGrupo = requestPesquisarCompatibilidades.addChildElement("codigoGrupo", "grup");
+        codigoGrupo.addTextNode("05");
+
+        SOAPElement codigoSubgrupo = requestPesquisarCompatibilidades.addChildElement("codigoSubgrupo", "sub");
+        codigoSubgrupo.addTextNode("05");
+
+        //Paginacao
+        SOAPElement paginacao = requestPesquisarCompatibilidades.addChildElement("Paginacao", "pag");
+        SOAPElement registroInicial = paginacao.addChildElement("registroInicial", "pag");
+        registroInicial.addTextNode("01");
+        SOAPElement quantidadeRegistros = paginacao.addChildElement("quantidadeRegistros", "pag");
+        quantidadeRegistros.addTextNode("20");
     }
 }
