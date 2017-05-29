45fdadf8d9ceb7b6340797c6513645ffbeef4779
Unknown <ibruno.om@gmail.com>
2017-05-29 13:37:20 -0300

    Adição de método de XML parse na Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 
 import br.com.integracaosigtap.connect.Connection;
 import br.com.integracaosigtap.connect.ConnectionSUS;
-import br.com.integracaosigtap.model.Envelope;
+import br.com.integracaosigtap.model.Procedimento;
 
 /**
  * Created by astr1x on 16/04/17.
@@ -13,20 +17,48 @@ import br.com.integracaosigtap.model.Envelope;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+    	
+    	
+    	XMLInputFactory fabrica = XMLInputFactory.newFactory();
 
         Connection connection = new ConnectionSUS();
         
-        JAXBContext jc = JAXBContext.newInstance(Envelope.class);
-        
-        Unmarshaller unmarshaller = jc.createUnmarshaller();
         
         System.out.println(connection.getPesquisarProcedimentos(ConnectionSUS.URL_PROCEDIMENTOS));
         
         StringReader rs = new StringReader(connection.getPesquisarProcedimentos(ConnectionSUS.URL_PROCEDIMENTOS));
-        Envelope procedimento = (Envelope) unmarshaller.unmarshal(rs);
-        System.out.println(procedimento);
         
-      
+        XMLEventReader reader = fabrica.createXMLEventReader(rs);
+        
+        Procedimento procedimento = null;
+        List<Procedimento> procedimentos = new ArrayList<Procedimento>();
+        
+        while(reader.hasNext()){
+        	 XMLEvent event = reader.nextEvent();
+        	 
+        	 if (event.isStartElement()){
+        		 StartElement startElement = event.asStartElement();
+        		 
+        		 if (startElement.getName().getLocalPart().equals("BaseProcedimento")){
+        			 procedimento = new Procedimento();
+        		 } else if (startElement.getName().getLocalPart().equals("codigo")){
+        			 event = reader.nextEvent();
+        			 System.out.println(event.asCharacters().getData());
+        			 procedimento.setCodigo(event.asCharacters().getData());
+        		 } else if (startElement.getName().getLocalPart().equals("nome")){
+        			 event = reader.nextEvent();
+        			 System.out.println(event.asCharacters().getData());
+        			 procedimento.setNome(event.asCharacters().getData());
+        		 }
+        	 }
+        	 
+        	 if (event.isEndElement()){
+        		 procedimentos.add(procedimento);
+        	 }
+        	 
+        }
+        
+        System.out.println(procedimentos.size());
 
 //        System.out.println(connection.getDetalharProcedimentos(ConnectionSUS.URL_PROCEDIMENTOS));
 
