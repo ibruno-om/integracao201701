0c9aeb06d118d89c9902f6e5644fb05a50871015
Unknown <ibruno.om@gmail.com>
2017-06-25 23:54:12 -0300

    Adicionar Anotação
    Adicionar Teste das classes FormaOrganizacao, ListarGrupos e ListarSubGrupos

--- src/main/java/br/com/integracaosigtap/model/Grupo.java
+++ src/main/java/br/com/integracaosigtap/model/Grupo.java
@@ -1,8 +1,11 @@
 package br.com.integracaosigtap.model;
 
+import br.com.integracaosigtap.model.handler.annotation.XMLClass;
+
 /**
  * Created by astr1x on 21/05/17.
  */
+@XMLClass(nodeName = "grup:Grupo", codigo = "ns4:codigo", nome = "ns4:nome")
 public class Grupo extends Model {
 
 	private Grupo grupoParent;
