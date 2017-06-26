e82d73f480c869e9b3b95ab66bf63eab82a98673
Unknown <leonardor.campos1@gmail.com>
2017-06-26 18:43:52 -0300

    Revert "Auto stash before merge of "Teste" and "origin/Teste""
    This reverts commit 9fc0a22f10434a96f18000adf46ca20d4dcbd74c.

--- src/test/java/br/com/integracaosigtap/connect/RequestListarFormaOrganizacaoTest.java
+++ src/test/java/br/com/integracaosigtap/connect/RequestListarFormaOrganizacaoTest.java
@@ -1,42 +1,3 @@
-package br.com.integracaosigtap.connect;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-import org.junit.Assert;
-
-/**
- * Created by Leonard0Rocha on 26/06/17.
- */
-public class RequestListarFormaOrganizacaoTest {
-
-	private String URL_COMPATIBILIDADES = "https://servicoshm.saude.gov.br/sigtap/CompatibilidadePossivelService/v1";
-
-	@Test
-	public void testListarFormaOrganizacao() throws Exception {
-		Connection connection = new RequestListarFormaOrganizacao();
-		String resposta = connection.getListarFormaOrganizacao(URL_COMPATIBILIDADES);
-		
-		System.out.println("Imprimindo o Teste a Partir daqui: \n\n\n\n" + resposta);
-		
-		String esperado = "requestListarFormaOrganizacao";
-		Assert.assertTrue(resposta.contains(esperado));
-		
-	}
-	@Test
-	public void testListarFormaOrganizacaoErro() throws Exception {
-		Connection connection = new RequestListarFormaOrganizacao();
-		String resposta = connection.getListarFormaOrganizacao(URL_COMPATIBILIDADES);
-		
-		String esperado = "requestNotMatch";
-		Assert.assertFalse(resposta.contains(esperado));
-		
-		esperado = "respondeNotMatch";
-		Assert.assertFalse(resposta.contains(esperado));
-	}
-
-}
 package br.com.integracaosigtap.connect;
 
 import org.junit.Test;
@@ -56,6 +17,8 @@ public class RequestListarFormaOrganizacaoTest {
 	public void testListarFormaOrganizacao() throws Exception {
 		Connection connection = new RequestListarFormaOrganizacao();
 		String resposta = connection.getListarFormaOrganizacao(URL_COMPATIBILIDADES);
+		
+		System.out.println("Imprimindo o Teste a Partir daqui: \n\n\n\n" + resposta);
 		
 		String esperado = "requestListarFormaOrganizacao";
 		Assert.assertTrue(resposta.contains(esperado));
