55d8257d1ec5e709f622c19e77be51181a8b6247
Unknown <ibruno.om@gmail.com>
2017-06-27 00:55:11 -0300

    Remoção de classe ModelHandler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,7 @@
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import br.com.integracaobsus.impl.BSus;
-import br.com.integracaosigtap.connect.ConnectionSUS;
+import br.com.integracaosigtap.model.Compatibilidade;
+import br.com.integracaosigtap.model.Grupo;
 import br.com.integracaosigtap.model.SubGrupo;
-import br.com.integracaosigtap.model.handler.SubGrupoHandler;
 
 /**
  * Created by astr1x on 16/04/17.
@@ -19,19 +13,16 @@ public class Main {
 		BSus bsus = new BSus();
 		bsus.setProperties(null);
 
-		SAXParserFactory parserFactor = SAXParserFactory.newInstance();
-		SAXParser parser = parserFactor.newSAXParser();
-		SubGrupoHandler handler = new SubGrupoHandler();
-
-		String xml = new ConnectionSUS()
-				.getListarGrupos("https://servicoshm.saude.gov.br/sigtap/NivelAgregacaoService/v1");
-		
-		System.out.println(xml);
-
-		parser.parse(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)), handler);
-
 		for (SubGrupo emp : bsus.listarSubGrupos()) {
 			System.out.println(emp);
+		}
+
+		for (Grupo grupo : bsus.listarGrupos()) {
+			System.out.println(grupo);
+		}
+
+		for (Compatibilidade compatibilidade : bsus.pesquisarCompatibilidades()) {
+			System.out.println(compatibilidade);
 		}
 
 	}
--- src/main/java/br/com/integracaosigtap/model/handler/ModelHandler.java
+++
@@ -1,9 +0,0 @@
-package br.com.integracaosigtap.model.handler;
-
-public class ModelHandler extends AbstractXMLHandler {
-
-	public ModelHandler(Class classType) {
-		super(classType);
-	}
-
-}
